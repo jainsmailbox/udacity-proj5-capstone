1e79ccac0ae680848467f1d4958c5b7a5161f778
jainsmailbox <142251515+jainsmailbox@users.noreply.github.com>
2023-09-29 22:40:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return """
-	       <h1 style='color: green;'>This is my capstone project version 1 by (Anurag jain)</h1>
+	       <h1 style='color: green;'>This is my capstone project version 1.1 by (Anurag jain)</h1>
            """
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
